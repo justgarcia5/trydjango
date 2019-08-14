bc5ed77f6ba49ee20264a1b544501fd500f1268d
justgarcia5 <justgarcia5@gmail.comgit config --global user.email justgarcia5@gmail.com>
2019-08-13 21:31:57 -0700

    cleaned up veiws

--- products/views.py
+++ products/views.py
@@ -4,10 +4,6 @@ from .models import Product
 # Create your views here.
 def product_detail_view(request):
   obj = Product.objects.get(id=1)
-  # context = {
-  #   "title": obj.title,
-  #   "description": obj.description,
-  # }
   context = {
     "obj": obj
   }
